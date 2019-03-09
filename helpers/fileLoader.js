2889c4b9c7a7fb6141af9570b40d3d3504ae6290
Sebastian Szczepański <s.szczepanski00@gmail.com>
2019-03-09 09:47:43 +0100

    Add fileLoader helper

---
+++ helpers/fileLoader.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+function loadFiles(directory) {
+  // Get path of directory containing files
+  const dir = path.join(__dirname, directory);
+
+  // Iterate through every file in directory
+  const files = fs.readdirSync(dir).map(file => {
+    // Get path of file, its name and extension
+    const filepath = path.join(dir, file);
+    const [filename, extension] = file.split('.');
+
+    // Accept only .js extensions
+    if (extension !== 'js') return {};
+
+    return {
+      filename,
+      ...require(filepath) // eslint-disable-line import/no-dynamic-require
+    };
+  });
+
+  // Remove empty objects and return array
+  return files.filter(obj => Object.keys(obj).length);
+}
+
+module.exports = loadFiles;
